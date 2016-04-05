14db14296cca6e8b7440eece2a55cedc74b0b3f1
Joshua Gustafson and lorenzo ferrario <silversevenfox@yahoo.com>
2016-04-05 10:48:57 -0700

    add bills information

--- app/routes/results/legislator.js
+++ app/routes/results/legislator.js
@@ -2,9 +2,15 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
-    var url = 'http://congress.api.sunlightfoundation.com/committees?&apikey=120aae8128344cd997a334cf42aa9403&member_ids=' + params.bioguide_id;
+    var committeeUrl = 'http://congress.api.sunlightfoundation.com/committees?&apikey=120aae8128344cd997a334cf42aa9403&member_ids=' + params.bioguide_id;
+    var billUrl = 'http://congress.api.sunlightfoundation.com/bills?&apikey=120aae8128344cd997a334cf42aa9403&sponsor_id=' + params.bioguide_id;
     var committeeRequest = function(){
-      return Ember.$.getJSON(url).then(function(responseJSON) {
+      return Ember.$.getJSON(committeeUrl).then(function(responseJSON) {
+        return responseJSON.results;
+      });
+    };
+    var billRequest = function(){
+      return Ember.$.getJSON(billUrl).then(function(responseJSON) {
         return responseJSON.results;
       });
     };
@@ -12,7 +18,8 @@ export default Ember.Route.extend({
       legislator: this.modelFor('results').filter(function(result){
         return result.bioguide_id === params.bioguide_id;
       }),
-      committees: committeeRequest()
+      committees: committeeRequest(),
+      bills: billRequest()
     });
 }
 });
